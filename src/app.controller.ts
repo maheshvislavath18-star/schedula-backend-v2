4b73ff870327917434920c29eabdd15ef8430a56
Mahesh <maheshvislavath18@gmail.com>
2026-06-18 13:14:25 +0530

    Add root route for Render

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  home() {
+    return {
+      status: 'success',
+      project: 'Schedula Backend V2',
+      message: 'Schedula Backend API Running Successfully',
+    };
   }
 }
